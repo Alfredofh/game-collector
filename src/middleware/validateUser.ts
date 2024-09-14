af20e55bc5d68f2312814e3c998bc626455c376f
DESKTOP-7T5FV20\alfre <alfredofweb@gmail.com>
2024-09-14 18:39:26 +0200

    feat(Register): Added email format validation and password length check to validateUser middleware

--- src/middleware/validateUser.ts
+++ src/middleware/validateUser.ts
@@ -1,10 +1,24 @@
 import { Request, Response, NextFunction } from 'express';
 
 export const validateUser = (req: Request, res: Response, next: NextFunction) => {
-    console.log('Request Body:', req.body);
     const { username, email, password } = req.body;
+
+    console.log('Datos recibidos para validación:', { username, email, password });
+
     if (!username || !email || !password) {
         return res.status(400).json({ message: 'Todos los campos son obligatorios' });
     }
+
+    // Validar formato del email
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+        return res.status(400).json({ message: 'El formato del correo electrónico no es válido' });
+    }
+
+    // Validar longitud de la contraseña
+    if (password.length < 6) {
+        return res.status(400).json({ message: 'La contraseña debe tener al menos 6 caracteres' });
+    }
+
     next();
 };
