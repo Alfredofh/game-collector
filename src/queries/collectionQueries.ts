4c419ae9531e11673c9d29a8b6b955cc55e22d76
DESKTOP-7T5FV20\alfre <alfredofweb@gmail.com>
2024-12-15 11:29:49 +0100

    feat(Get collections): Added getCollectionsController and updated collectionQueries to include getCollectionByUser function

--- src/queries/collectionQueries.ts
+++ src/queries/collectionQueries.ts
@@ -1,12 +1,21 @@
 import connection from '../db';
 import { Collection } from '../models/Collection';
-import { ResultSetHeader } from 'mysql2/promise';
+import { ResultSetHeader, RowDataPacket } from 'mysql2/promise';
 
 // Crear una nueva colección
 export const createCollection = async (collection: Collection): Promise<number> => {
     const [result] = await connection.promise().query<ResultSetHeader>(
-        'INSERT INTO Collections (name, user_id) VALUES (?, ?)', 
+        'INSERT INTO Collections (name, user_id) VALUES (?, ?)',
         [collection.name, collection.user_id]
     );
     return result.insertId;  // Devolvemos el ID de la colección recién creada
+};
+
+//Obtener todas las colecciones de un usuario
+
+export const getCollectionByUser = async (userId: number): Promise<Collection[]> => { // Promise<Collection[]>: Indica que la función es asíncrona y devuelve una promesa que, cuando se resuelve, contiene un array de objetos del tipo Collection
+    const [rows] = await connection.promise().query<Collection[] & RowDataPacket[]>(
+        'SELECT * FROM Collections WHERE user_id = ?', [userId]
+    );
+    return rows;
 };
--- src/routes/collectionRoutes.ts
+++ src/routes/collectionRoutes.ts
@@ -1,10 +1,11 @@
 // src/routes/collectionRoutes.ts
 import { Router } from 'express';
-import { createCollectionController } from '../controllers/collectioncontroller';
+import { createCollectionController, getCollectionsController } from '../controllers/collectioncontroller';
 import { authenticateJWT } from '../middleware/authenticateJWT';
 const router = Router();
 
 // Ruta para crear una nueva colección
 router.post('/api/collection', authenticateJWT, createCollectionController);
+router.get('/api/collection', authenticateJWT, getCollectionsController);
 
 export default router;
