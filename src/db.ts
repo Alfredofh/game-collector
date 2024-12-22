20dcab4f094d90ad4c8a2c74bdbe02fb28806a6b
DESKTOP-7T5FV20\alfre <alfredofweb@gmail.com>
2024-12-22 16:09:43 +0100

    refactor(DB): change database from Mysql to postgreSql.

--- src/db.ts
+++ src/db.ts
@@ -1,21 +1,22 @@
-import mysql from 'mysql2';
+import { Pool } from 'pg';
 import dotenv from 'dotenv';
 
-dotenv.config();  // Cargar las variables de entorno
+dotenv.config();
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+const pool = new Pool({
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: Number(process.env.PG_PORT),
 });
 
-connection.connect((err: mysql.QueryError | null) => {
-  if (err) {
-    console.error('Error de conexión a MySQL:', err);
-    return;
-  }
-  console.log('Conectado a la base de datos MySQL');
+pool.on('connect', () => {
+  console.log('Conectado a PostgreSQL');
 });
 
-export default connection;
+pool.on('error', (err) => {
+  console.error('Error en la conexión con PostgreSQL:', err);
+});
+
+export default pool;
