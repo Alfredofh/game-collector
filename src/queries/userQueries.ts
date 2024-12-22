f2eca6a61fcb5c44950ff414b9528086d42b7ab0
DESKTOP-7T5FV20\alfre <alfredofweb@gmail.com>
2024-12-22 16:55:35 +0100

    refactor(): Update queries for use PostgreSQL and pool

--- src/queries/userQueries.ts
+++ src/queries/userQueries.ts
@@ -1,21 +1,25 @@
-import connection from '../db';
+import pool from '../db';
 import { User } from '../models/User';
-import { RowDataPacket, ResultSetHeader } from 'mysql2/promise';
 
 // Verificar si el usuario ya existe
 export const findUserByEmail = async (email: string): Promise<User[]> => {
-    const [rows] = await connection.promise().query<User[] & RowDataPacket[]>(
-        'SELECT * FROM Users WHERE email = ?', 
-        [email]
-    );
-    return rows; 
+    const query = `
+        SELECT * FROM Users
+        WHERE email = $1;
+    `;
+    const values = [email];
+    const result = await pool.query(query, values);
+    return result.rows;
 };
 
 // Crear un nuevo usuario
 export const createUser = async (user: User): Promise<number> => {
-    const [result] = await connection.promise().query<ResultSetHeader>(
-        'INSERT INTO Users (name, email, password_hash) VALUES (?, ?, ?)',
-        [user.name, user.email, user.password_hash]
-    );
-    return result.insertId;
+    const query = `
+        INSERT INTO Users (name, email, password_hash)
+        VALUES ($1, $2, $3)
+        RETURNING id;
+    `;
+    const values = [user.name, user.email, user.password_hash];
+    const result = await pool.query(query, values);
+    return result.rows[0].id;
 };
