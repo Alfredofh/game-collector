0f5c6dbb2b5be564dc63227b0564a56254104f59
DESKTOP-7T5FV20\alfre <alfredofweb@gmail.com>
2024-12-28 21:45:24 +0100

    feat(CRUD Games): Add update games

--- src/controllers/gameController.ts
+++ src/controllers/gameController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { addGameToCollection } from '../queries/gameQueries';
+import { addGameToCollection, updateGame } from '../queries/gameQueries';
 import { Game } from '../models/Game';
 
 
@@ -29,6 +29,38 @@ export const createGameController = async (req: Request, res: Response) => {
         res.status(201).json({ message: 'Videojuego añadido exitosamente', gameId });
     } catch (error) {
         console.error('Error al añadir el videojuego:', error);
+        res.status(500).json({ message: 'Error interno del servidor' });
+    }
+};
+
+//Update a game
+export const updateGameController = async (req: Request, res: Response) => {
+    const { name, platform, release_year, value, upc, ean, description, image_url, collection_id } = req.body;
+    const gameId = parseInt(req.params.id, 10);
+
+    if (!name || !collection_id || isNaN(gameId)) {
+        return res.status(400).json({ message: 'El nombre del juego y el ID de la colección son obligatorios' });
+    }
+
+    try {
+        const updatedGame: Game = {
+            name,
+            platform,
+            release_year,
+            value,
+            upc,
+            ean,
+            description,
+            image_url,
+            collection_id,
+        };
+
+        // Llamamos a la consulta para actualizar el videojuego
+        const updatedGameId = await updateGame(updatedGame, gameId);
+
+        res.status(200).json({ message: 'Videojuego actualizado exitosamente', updatedGameId });
+    } catch (error) {
+        console.error('Error al actualizar el videojuego:', error);
         res.status(500).json({ message: 'Error interno del servidor' });
     }
 };
--- src/routes/gameRoutes.ts
+++ src/routes/gameRoutes.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
-import { createGameController } from '../controllers/gameController';
+import { createGameController, updateGameController } from '../controllers/gameController';
 import { authenticateJWT } from '../middleware/authenticateJWT';
 
 const router = Router();
 
 // Ruta para añadir un videojuego a una colección
 router.post('/api/videogames/add', authenticateJWT, createGameController);
-
+router.put('/api/videogames/update/:id', authenticateJWT, updateGameController);
 export default router;
