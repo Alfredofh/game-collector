260e6a168be42db08b4221c2831a2dc9a2b90f51
DESKTOP-7T5FV20\alfre <alfredofweb@gmail.com>
2025-01-17 18:08:52 +0100

    refactor(Auth): adds checks to middleware and more specific messages

--- src/routes/gameRoutes.ts
+++ src/routes/gameRoutes.ts
@@ -7,5 +7,5 @@ const router = Router();
 // Ruta para añadir un videojuego a una colección
 router.post('/api/videogames/add', authenticateJWT, createGameController);
 router.put('/api/videogames/update/:id', authenticateJWT, updateGameController);
-router.delete('/api/videogames/:id', authenticateJWT, deleteGameByIdController);
+router.delete('/api/videogames/remove/:id', authenticateJWT, deleteGameByIdController);
 export default router;
